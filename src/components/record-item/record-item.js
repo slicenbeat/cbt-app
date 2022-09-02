680df5dbb33ac186fc491281eb9441400766b2e7
Damir Ivaev <slicenbeat@gmail.com>
2022-09-02 11:46:03 +0400

    feat: add edit mode for record (without request)

--- src/components/record-item/record-item.js
+++ src/components/record-item/record-item.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-
+import trash from "../../assets/icons/trash.svg";
+import edit from "../../assets/icons/edit.svg";
+import check from "../../assets/icons/check.svg";
+import { useState } from "react";
 const Li = styled.li`
   list-style-type: none;
 `;
 const RecordListBlock = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   border: 1px solid #919acc;
-  width: 1000px;
+  width: 800px;
   padding: 20px;
   margin: 5px;
   background-color: #ffffff;
@@ -15,13 +21,109 @@ const RecordListBlock = styled.div`
     color: #3f3a3a;
   }
 `;
-
+const TextBlock = styled.div``;
+const ButtonBlock = styled.div`
+  align-self: center;
+  margin-left: 5px;
+  background-color: transparent;
+  img,
+  button {
+    background-color: transparent;
+    width: 16px;
+  }
+  button {
+    border: none;
+`;
+const ButtonsBlock = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: white;
+`;
+const TextArea = styled.textarea`
+  padding-top: 100px;
+  max-width: 600px;
+  min-width: 600px;
+  min-height: 50px;
+  max-height: 150px;
+  padding: 10px;
+  border: 1px solid #919acc;
+  ::placeholder {
+    text-align: center;
+  }
+  &:active {
+    border: 1px solid #919acc;
+  }
+  &:focus {
+    border: 1px solid #919acc;
+    outline: none;
+  }
+`;
 const RecordListItem = ({ thoughts, emotions }) => {
+  const [isEditMode, setEditMode] = useState(false);
+  const [editedThoughts, setEditedThoughts] = useState(thoughts);
+  const [editedEmotions, setEditedEmotions] = useState(emotions);
   return (
     <Li>
       <RecordListBlock>
-        <p>Мысли: {thoughts}</p>
-        <p>Эмоции: {emotions}</p>
+        <TextBlock>
+          <p>
+            Мысли:&nbsp;
+            {isEditMode ? (
+              <TextArea
+                value={editedThoughts}
+                onChange={(e) => {
+                  setEditedThoughts(e.target.value);
+                }}
+              ></TextArea>
+            ) : (
+              <span>{editedThoughts}</span>
+            )}
+          </p>
+          <p>
+            Эмоции:&nbsp;
+            {isEditMode ? (
+              <input
+                type={"text"}
+                value={editedEmotions}
+                onChange={(e) => {
+                  setEditedEmotions(e.target.value);
+                }}
+              />
+            ) : (
+              <span>{editedEmotions}</span>
+            )}
+          </p>
+        </TextBlock>
+        <ButtonsBlock>
+          <ButtonBlock>
+            {isEditMode ? (
+              <button
+                onClick={() => {
+                  setEditMode(!isEditMode);
+                }}
+              >
+                <img src={check} alt="Иконка для сохранения" />
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  setEditMode(!isEditMode);
+                }}
+              >
+                <img src={edit} alt="Иконка для редактирования" />
+              </button>
+            )}
+          </ButtonBlock>
+          <ButtonBlock>
+            <button
+              onClick={() => {
+                console.log("Удалить");
+              }}
+            >
+              <img src={trash} alt="Иконка для удаления" />
+            </button>
+          </ButtonBlock>
+        </ButtonsBlock>
       </RecordListBlock>
     </Li>
   );
