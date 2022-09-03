96813c06f5ad4858c5df943c91c4b818003fa154
Damir Ivaev <slicenbeat@gmail.com>
2022-09-03 09:01:14 +0400

    fix: add deploy server links

--- src/asyncActions/records.js
+++ src/asyncActions/records.js
@@ -8,28 +8,40 @@ import axios from "axios";
 export const fetchRecords = () => {
   return function (dispatch) {
     axios
-      .get("http://localhost:3001/records")
+      .get(
+        "https://my-json-server.typicode.com/slicenbeat/cbt-app-server/records"
+      )
       .then((response) => dispatch(getManyRecordsAction(response.data)));
   };
 };
 export const postRecord = (data) => {
   return function (dispatch) {
     axios
-      .post("http://localhost:3001/records", data)
+      .post(
+        "https://my-json-server.typicode.com/slicenbeat/cbt-app-server/records",
+        data
+      )
       .then((response) => dispatch(addRecordAction(response.data)));
   };
 };
 export const editRecord = (data) => {
   return function (dispatch) {
     axios
-      .put(`http://localhost:3001/records/${data.id}`, data)
+      .put(
+        `https://my-json-server.typicode.com/slicenbeat/cbt-app-server/records/${data.id}`,
+        data
+      )
       .then((response) => dispatch(editRecord(response.data)));
   };
 };
 export const deleteRecord = (id) => {
   return function (dispatch) {
-    axios.delete(`http://localhost:3001/records/${id}`).then((response) => {
-      dispatch(removeRecordAction(id));
-    });
+    axios
+      .delete(
+        `https://my-json-server.typicode.com/slicenbeat/cbt-app-server/records/${id}`
+      )
+      .then((response) => {
+        dispatch(removeRecordAction(id));
+      });
   };
 };
